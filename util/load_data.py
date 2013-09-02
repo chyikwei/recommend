e46904f99cc38288c36b909842d658627b8524ff
Chyi-Kwei Yau <chyikwei.yau@getglue.com>
2013-09-02 18:53:07 -0400

    add build matrix

--- util/load_data.py
+++ util/load_data.py
@@ -12,7 +12,7 @@ def load_ml_1m():
     """
     num_user = 6040
     num_item = 3952
-
+    print("\nloadind movie lens 1M data")
     with open("data/ratings.dat", "rb") as f:
         iter_lines = iter(f)
         ratings = []
@@ -22,16 +22,55 @@ def load_ml_1m():
             line = [int(l) for l in line]
             ratings.append(line)
 
-            if line_num % 10000 == 0:
+            if line_num % 100000 == 0:
                 print line_num
 
     ratings = np.array(ratings)
 
     # shift user_id & movie_id by 1. let user_id & movie_id start from 0 
     ratings[:,(0, 1)] = ratings[:, (0, 1)] - 1
-    
+    print "max user id", max(ratings[:, 0])
+    print "max item id", max(ratings[:, 1])
     return num_user, num_item, ratings
 
 
 def build_matrix(num_user, num_item, ratings):
-    matrix = sparse.lil_matrix(num_user, num_item, dtype='float16')
+    print '\nbuild matrix'
+    # sparse matrix
+    #matrix = sparse.lil_matrix((num_user, num_item))
+    # dense matrix
+    matrix = np.zeros((num_user, num_item), dtype='int8')
+    for item_id in xrange(num_item):
+        data = ratings[ratings[:, 1] == item_id]
+        if data.shape[0] > 0:
+            matrix[data[:, 0], item_id] = data[:, 2]
+
+        if item_id % 1000 == 0:
+            print item_id
+
+    return matrix
+
+def build_sparse_matrix(num_user, num_item, ratings):
+    print '\nbuild sparse matrix'
+    # sparse matrix
+    matrix = sparse.lil_matrix((num_user, num_item))
+    for item_id in xrange(num_item):
+        data = ratings[ratings[:, 1] == item_id]
+        if data.shape[0] > 0:
+            #for sparse matrix
+            matrix[data[:, 0], item_id] = np.array([data[:, 2]]).T            
+
+        if item_id % 1000 == 0:
+            print item_id
+
+    return matrix
+
+def test():
+    num_user, num_item, ratings = load_ml_1m()
+    matrix = build_matrix(num_user, num_item, ratings)
+    print 'matrix size', matrix.shape
+    
+    spase_matrix = build_sparse_matrix(num_user, num_item, ratings)
+    print 'sparse matrix size', sparse_matrix.shape
+    
+
