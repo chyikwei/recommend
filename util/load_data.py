35d2855b151f84f996a1d158feb86ed0d78a785e
Chyi-Kwei Yau <chyikwei.yau@getglue.com>
2013-09-07 10:21:47 -0400

    load gzip data

--- util/load_data.py
+++ util/load_data.py
@@ -5,9 +5,11 @@ load data set
 import numpy as np
 import scipy.sparse as sparse
 
-def load_ml_1m():
+
+def build_ml_1m():
     """
-    load movie lens 1M rating
+    build movie lens 1M ratings from original ml_1m rating file.
+    need to download and put ml_1m data in /data folder first.
     Source: http://www.grouplens.org/
     """
     num_user = 6040
@@ -27,14 +29,35 @@ def load_ml_1m():
 
     ratings = np.array(ratings)
 
-    # shift user_id & movie_id by 1. let user_id & movie_id start from 0 
-    ratings[:,(0, 1)] = ratings[:, (0, 1)] - 1
+    # shift user_id & movie_id by 1. let user_id & movie_id start from 0
+    ratings[:, (0, 1)] = ratings[:, (0, 1)] - 1
     print "max user id", max(ratings[:, 0])
     print "max item id", max(ratings[:, 1])
     return num_user, num_item, ratings
 
 
-def build_matrix(num_user, num_item, ratings):
+def load_ml_1m():
+    """load Movie Lens 1M ratings from saved gzip file"""
+    import gzip
+    import cPickle
+
+    file_path = 'data/ratings.gz'
+    with gzip.open(file_path, 'rb') as f:
+        print "load ratings from: %s" % file_path
+        num_user = cPickle.load(f)
+        num_item = cPickle.load(f)
+        ratings = cPickle.load(f)
+
+        return num_user, num_item, ratings
+
+
+def build_rating_matrix(num_user, num_item, ratings):
+    """
+    build dense ratings matrix from original ml_1m rating file.
+    need to download and put ml_1m data in /data folder first.
+    Source: http://www.grouplens.org/
+    """
+
     print '\nbuild matrix'
     # sparse matrix
     #matrix = sparse.lil_matrix((num_user, num_item))
@@ -50,27 +73,42 @@ def build_matrix(num_user, num_item, ratings):
 
     return matrix
 
+
+def load_rating_matrix():
+    """
+    load Movie Lens 1M ratings from saved gzip file
+    Format is numpy dense matrix
+    """
+    import gzip
+    import cPickle
+
+    file_path = 'data/rating_matrix.gz'
+    with gzip.open(file_path, 'rb') as f:
+        print "load ratings matrix from: %s" % file_path
+        return cPickle.load(f)
+
+
 def build_sparse_matrix(num_user, num_item, ratings):
+    # TODO: have not tested it yet. will test after algorithm support sparse matrix
     print '\nbuild sparse matrix'
     # sparse matrix
     matrix = sparse.lil_matrix((num_user, num_item))
     for item_id in xrange(num_item):
         data = ratings[ratings[:, 1] == item_id]
         if data.shape[0] > 0:
-            #for sparse matrix
-            matrix[data[:, 0], item_id] = np.array([data[:, 2]]).T            
+            # for sparse matrix
+            matrix[data[:, 0], item_id] = np.array([data[:, 2]]).T
 
         if item_id % 1000 == 0:
             print item_id
 
     return matrix
 
+
 def test():
     num_user, num_item, ratings = load_ml_1m()
     matrix = build_matrix(num_user, num_item, ratings)
-    print 'matrix size', matrix.shape
-    
+    print 'dense matrix size', matrix.shape
+
     spase_matrix = build_sparse_matrix(num_user, num_item, ratings)
     print 'sparse matrix size', sparse_matrix.shape
-    
-
