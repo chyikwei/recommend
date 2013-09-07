29c6fe6ef1a176254d0e3f73242ba4070271a65c
Chyi-Kwei Yau <chyikwei.yau@getglue.com>
2013-09-07 10:17:02 -0400

    change to ABC class

--- mf/base.py
+++ mf/base.py
@@ -2,29 +2,49 @@
 Base class of recommendation System
 """
 
+from abc import ABCMeta, abstractmethod
+
+
 class NotImplementedError(Exception):
+
     def __init__(self):
         super(NotImplementedError, self).__init__()
 
 
+class DimensionError(Exception):
+
+    def __init__(self):
+        super(DimensionError, self).__init__()
+
+
 class Base(object):
+
     """base class of recommendations"""
+
+    __metaclass__ = ABCMeta
+
     def __init__(self):
         self.train_errors = []
         self.validation_erros = []
 
-    def Estimate(self, iter=1000):
+    @abstractmethod
+    def estimate(self, iter=1000):
+        """training models"""
         raise NotImplementedError
 
+    @abstractmethod
     def suggestions(self, user_id, num=10):
+        """suggest items for given user"""
         raise NotImplementedError
 
-    def save_model(self):
+    @abstractmethod
+    def save_model(self, path):
         raise NotImplementedError
 
     @classmethod
-    def load_model(cls, file):
-        raise NotImplementedError          
+    def load_model(cls, path):
+        """load saved models"""
+        raise NotImplementedError
 
     def __reper__(self):
         return self.__class__.__name__
